dba887447257f9e372e648f502e4db9ffe747d3f
AndrewDyomin <dyomin.andrew1@gmail.com>
2025-05-08 08:49:12 +0300

    Update BarcodeScanner.jsx

--- src/app/components/BarcodeScanner.jsx
+++ src/app/components/BarcodeScanner.jsx
@@ -35,7 +35,10 @@ const BarcodeScanner = ({ productsArray }) => {
           (result, err) => {
             if (result) {
               const target = productsArray.filter(product => product.barcode === result.text)
-              document.getElementById("result").textContent = target[0]?.name ? `${target[0].name}(${target[0].article})` : 'Товар не найден'
+              document.getElementById("result").textContent = target[0]?.name ? 
+                `${target[0].name}(${target[0].article})` 
+                : 
+                `Товар ${result.text} не найден`
             }
             if (err && !(err instanceof NotFoundException)) {
               console.error(err);
